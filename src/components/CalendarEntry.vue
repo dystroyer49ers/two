f03a3d7b43f45319c1efbdf57bd653ede1c79671
dystroyer49ers <34371700+dystroyer49ers@users.noreply.github.com>
2019-03-11 19:13:41 +0000

    Initial commit

---
+++ src/components/CalendarEntry.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+	<div id="calender-entery">
+		<!-- Markup for the ccalender entery -->
+	</div> 
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.day-event {
+  margin-top: 6px;
+  margin-bottom: 6px;
+  display: block;
+  color: #4C4C4C;
+  padding: 5px;
+
+  .details {
+    display: block;
+  }
+
+  input {
+    background: none;
+    border: 0;
+    border-bottom: 1px solid #FFF;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
